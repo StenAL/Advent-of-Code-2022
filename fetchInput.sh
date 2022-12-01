83dc378dd6574777ddd43b0d2085650c53ca0b70
Sten Laane <21343173+StenAL@users.noreply.github.com>
2022-12-01 08:39:01 +0000

    Add auxiliary files from last year

---
+++ fetchInput.sh
@@ -0,0 +1,19 @@
+day=$1
+inputFile="src/input/day$day.txt"
+if [[ ! -f $inputFile ]]; then
+  curl -sS --cookie "session=$(cat cookie.txt)" "https://adventofcode.com/2022/day/$day/input" -o $inputFile
+  echo "Fetched $inputFile"
+  git add $inputFile
+else
+  echo "$inputFile already exists"
+fi
+
+sourceFile="src/day$day.py"
+if [[ ! -f $sourceFile ]]; then
+  cp src/skeleton.py src/day$day.py
+  sed -i "s/'REPLACE_ME'/$day/g" src/day$day.py
+  echo "Created $sourceFile"
+  git add $sourceFile
+else
+  echo "$sourceFile already exists"
+fi
